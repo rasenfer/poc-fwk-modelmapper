c73b5f64c436072a407046a63ac0399ef196db72
Rubén Asensio Fernández <ruben.asenfer@gmail.com>
2019-06-26 10:56:36 +0200

    [REF] undo prevents lazy initialization error

--- src/main/java/poc/fwk/modelmapper/LazyLoadModelMapper.java
+++ src/main/java/poc/fwk/modelmapper/LazyLoadModelMapper.java
@@ -1,49 +1,17 @@
 package poc.fwk.modelmapper;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
-import org.hibernate.LazyInitializationException;
 import org.modelmapper.MappingException;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.internal.InheritingConfiguration;
-import org.modelmapper.internal.MappingEngineImpl;
 import org.modelmapper.spi.ErrorMessage;
-import org.modelmapper.spi.MappingContext;
-import org.springframework.util.ReflectionUtils;
 
 public class LazyLoadModelMapper extends ModelMapper {
 
 	public static final String LAZY_MODEL_MAPPER_THREAD_NAME = "_LAZY";
-
-	public LazyLoadModelMapper() {
-		Field configField = ReflectionUtils.findField(ModelMapper.class, "config");
-		ReflectionUtils.makeAccessible(configField);
-		InheritingConfiguration config = InheritingConfiguration.class.cast(ReflectionUtils.getField(configField, this));
-		MappingEngineImpl engine = new MappingEngineImpl(config) {
-			@Override
-			public <S, D> D map(MappingContext<S, D> context) {
-				D mapped;
-				try {
-					mapped = super.map(context);
-				} catch (MappingException e) {
-					Throwable cause = getTopException(e);
-					if (LazyInitializationException.class.isInstance(cause)) {
-						mapped = context.getDestination();
-					} else {
-						throw e;
-					}
-				}
-				return mapped;
-			}
-		};
-		Field engineField = ReflectionUtils.findField(ModelMapper.class, "engine");
-		ReflectionUtils.makeAccessible(engineField);
-		ReflectionUtils.setField(engineField, this, engine);
-	}
 
 	@Override
 	public <D> D map(Object source, Class<D> destinationType) {
@@ -128,13 +96,5 @@ public class LazyLoadModelMapper extends ModelMapper {
 		} catch (Exception e) {
 			throw new MappingException(Arrays.asList(new ErrorMessage(e.getMessage(), e)));
 		}
-	}
-
-	private Throwable getTopException(Throwable e) {
-		Throwable throwable = e;
-		if (e.getCause() != null) {
-			throwable = getTopException(e.getCause());
-		}
-		return throwable;
 	}
 }
